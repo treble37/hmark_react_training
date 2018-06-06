6b7ceb187127d7fc0057c032dcdc7b366d85b32b
Bruce Park <brucepark0@gmail.com>
2018-06-06 15:44:28 -0700

    Add Navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 import ECards from "./ECards";
 import Home from "./Home";
+import Navigation from "./Navigation";
 
 class App extends React.Component {
   render() {
     return (
       <Fragment>
+        <Navigation />
         <Home />
         <ECards />
       </Fragment>
---
+++ src/Navigation.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Navigation extends React.Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="https://www.google.com/">Google</a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Navigation;
